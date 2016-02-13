31516b10c389f877e74320d298b4997c1e53bc20
FNU Kshitij <Kshitij@FNUs-MacBook-Pro.local>
2016-02-13 12:43:37 -0700

    Removed Angular Folder

--- meedget_angular/scripts/controllers/common.js
+++
@@ -1,8 +0,0 @@
-/**
- * Created by hitenpratap on 13/01/16.
- */
-var commonApp = angular.module('commonApp', []);
-
-commonApp.controller('customMessageController', function ($scope, $routeParams) {
-    $scope.message = sessionStorage.getItem("userAuthMsg");
-});
